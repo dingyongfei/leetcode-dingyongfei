9c150bb61fe15dae5acb4e3ff801e2d84a532888
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-21 01:29:07 +0800

    Create 154_Find Minimum in Rotated Sorted Array II.java

---
+++ 154_Find Minimum in Rotated Sorted Array II.java
@@ -0,0 +1,18 @@
+//finished. Same as question 153.
+class Solution {
+	public int findMin(int[] arr) {
+		int low = 0;
+		int high = arr.length - 1;
+		while (low < high) { // 注：也可以写成while(low<=high)
+			int mid = low + (high - low) / 2;
+			if (arr[mid] > arr[high]) {
+				low = mid + 1;
+			} else if (arr[mid] == arr[high]) {
+				high = high - 1;
+			} else {
+				high = mid;
+			}
+		}
+	 	return arr[low];
+	}
+}
