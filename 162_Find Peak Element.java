1c8d685a3f11ca295aed9fb7358c5e3d492c6f7f
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-21 01:30:21 +0800

    Create 162_Find Peak Element.java

---
+++ 162_Find Peak Element.java
@@ -0,0 +1,53 @@
+//注：该题目的base case一共有四种情况，全部都要写出去！！
+class Solution {
+	public int findPeakElement(int[] nums) {
+		if (nums == null || nums.length == 0) {             //1. 数组为空
+			return -1;
+		}
+		if (nums.length == 1) {                             //2. 数组中只有一个元素，直接返回该元素下标
+			return 0;
+		}
+
+		if (nums[0] > nums[1]) {                            //3. 开头第一个元素比开头第二个元素大，直接返回第一个下标0
+			return 0;
+		}
+
+		if (nums[nums.length - 1] > nums[nums.length - 2]) { //4. 同理，  结尾最后一个元素比倒数第二个大，直接返回最后一个下标nums.length - 1.
+			return nums.length - 1;
+		}
+
+		int left = 1;                           //注意，left和right的下标值绝对不能从0和nums.length - 1开始，否则在while循环中会产生数组下标越界！！！
+		int right = nums.length - 2;
+		int mid = 0;
+		while (left <= right) {
+			mid = (left + right) / 2;
+			if (nums[mid] < nums[mid - 1]) {
+				right = mid - 1;
+			} else if (nums[mid] < nums[mid + 1]) {
+				left = mid + 1;
+			} else {
+				return mid;                         //否则，返回mid。
+			}
+		}
+		return left;
+	}
+}
+
+//简便做法 （因为题目有条件 ：（You may imagine that nums[-1] = nums[n] = -∞.）
+class Solution {
+	public int findPeakElement(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return -1;
+		}
+		if (arr.length == 1) {
+			return 0;
+		}
+		int i = 0;
+		for (; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				break;
+			}
+		}
+		return i;
+	}
+}
