fa897da3d77388be6de400efc24cf49736c96862
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-21 01:36:51 +0800

    Create 171_Excel Sheet Column Number.java

---
+++ 171_Excel Sheet Column Number.java
@@ -0,0 +1,16 @@
+// 题168 & 171重要小结: 和 字符串-> 整数 以及 整数 -> 字符串 的思想一模一样, 不同的是每次余数都要相应地加 1 和减 1！
+// 对应于题169。是题目169的相反过程。对应地，每次余数都 +1
+class Solution {
+	public int titleToNumber(String str) {
+		if (str == null || str.equals("")) {
+			return 0;
+		}
+		char[] c = str.toCharArray();
+		int res = 0;
+		for (int i = 0; i < c.length; i++) {
+			res = res * 26 + c[i] - 'A' + 1;           // 每次余数都 +1
+		}
+		return res;
+	}
+}
+//看，整个过程 和” 字符串和整数之间的相互转化 “是非常类似的， 区别就是该题相当于”26进制”的数字，而普通的字符串和整数之间的相互转化题是10进制。
