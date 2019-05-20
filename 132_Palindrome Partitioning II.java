b235a488d8d0713b9b3fead7d52be515119deade
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-21 01:16:08 +0800

    Update 132_Palindrome Partitioning II.java

--- 132_Palindrome Partitioning II.java
+++ 132_Palindrome Partitioning II.java
@@ -1,3 +1,4 @@
+//zuochengyun  chapter5 回文最小分割数
 // dp[i]的含义是子串str[i..len-1]至少需要切割几次，才能把str[i.. len-1]全部切成回文子串。那么，dp[O]就是最后的结果。
 //假设j位置处在i与len-1位置之间（i＜=j＜len)，如果str[i..j]是回文串，那么dp[i]的值可能是dp[j+1]+1
 // 让j在i到len-1位置上枚举，那么所有可能情况中的最小值就是dp[i]的值，即dp[i]= Min { dp[j+ 1]+ 1 (i<=j<len，且str[i..j]必须是回文串)}。
