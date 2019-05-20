b29d9e16d39ee11cd3f0fca84577a6afc9b63117
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-21 01:20:49 +0800

    Create 141_Linked List Cycle.java

---
+++ 141_Linked List Cycle.java
@@ -0,0 +1,18 @@
+//faster solution.
+class Solution {
+	public boolean hasCycle(ListNode head) {
+		if (head == null || head.next == null) {
+			return false;
+		}
+		ListNode fast = head;
+		ListNode slow = head;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+			if (fast == slow) {               //注意，只能写成fast == slow! (不能fast.val == fast.val)
+				return true;
+			}
+		}
+		return false;
+	}
+}
